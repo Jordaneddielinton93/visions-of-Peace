537d31a2d23d87cc2680c02e843d52d83c4c6a6d
School Of Code Staff <staff@kdp4pcflwr.home>
2022-11-20 16:30:59 +0000

    added carousel testing if bundler handles it

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,20 +1,22 @@
 import { Box } from "@mui/system";
 import Head from "next/head";
 import * as React from "react";
+import Carousel1 from "../components/Carousel/Carousel1";
+import { WhyChooseMe } from "../components/WhyChooseMe";
 
-export interface IStoreProps {}
-
-export function Store(props: IStoreProps) {
+export default function Store() {
   return (
     <Box bgcolor={"background"}>
       <Head>
-        <title>visions-of-peace</title>
+        <title>visions-of-peace:store</title>
         <meta
           name="description"
           content="We are dedicated to help you achieve a sense of well being for your body , soul and mind by providig a variety of health, beauty and mind services"
         />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <Carousel1 />
+      <WhyChooseMe />
     </Box>
   );
 }
